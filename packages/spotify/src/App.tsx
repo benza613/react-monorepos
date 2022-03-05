c0ad2a2cba9e02eb1601ef89bc171f514e86f13f
Benito Alvares <benito.alvares@gmail.com>
2022-03-05 02:09:57 -0600

    updated card layout #1

--- packages/spotify/src/App.tsx
+++ packages/spotify/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "./containers/shared/layout";
-
+import "./App.scss";
 
 const App = () => {
   return (
--- packages/spotify/src/containers/shared/layout.tsx
+++ packages/spotify/src/containers/shared/layout.tsx
@@ -5,33 +5,42 @@ import axios, { AxiosRequestConfig } from "axios";
 import Q from "../../business/utils/api";
 import mocks from "../../business/utils/mocks";
 
+import { Card } from "react-bootstrap";
+
+import "./layout.scss";
+
 const Layout = () => {
   useEffect(() => {
-    var config: AxiosRequestConfig = {
-      baseURL: "https://accounts.spotify.com",
-      url: "/authorize",
-    };
-
-    Q(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    // var config: AxiosRequestConfig = {
+    //   baseURL: "https://accounts.spotify.com",
+    //   url: "/authorize",
+    // };
+    // Q(config)
+    //   .then((response) => {
+    //     console.log(JSON.stringify(response.data));
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
   }, []);
 
   return (
-    <div className="row layout">
-      <div className="col">
-        {mocks.playlist.items.map((P) => (
-          <div className="col-12 playlist">
-            {P.name} <br />
-            Total: {P.tracks.total}
+    <div className="row layout g-4">
+      {mocks.playlist.items.map((p) => (
+        <div className="col-3">
+          <div className="card spCard h-100">
+            <img src={p.images?.[0].url} className="card-img-top" alt="" />
+
+            <div className="card-header">{p.name}</div>
+            <div className="card-body">
+              {p.description} <br />
+            </div>
+            <div className="card-footer">
+              <small className="text-muted">Total: {p.tracks.total}</small>
+            </div>
           </div>
-        ))}
-      </div>
-      <div className="col">Songs</div>
+        </div>
+      ))}
     </div>
   );
 };
